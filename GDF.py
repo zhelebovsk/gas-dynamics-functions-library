d0ea1426624657f8440c15c1a4c5f1331d7e9c1e
zhelebovsk <qrabeq@gmail.com>
2020-08-05 19:00:13 +0300

    direct r functions (t,p,e) 1st est la(q)

--- GDF.py
+++ GDF.py
@@ -1,14 +1,16 @@
 import math
-#перед использованием требуется проверить точность (числа Маха)
-#поиск обратных функций выполняется с заданой точностью
+import numpy as np
+
+# перед использованием требуется проверить точность (числа Маха)
+# поиск обратных функций выполняется с заданой точностью
 
 
 """
-1. переделать функции обратные температуры, давления, плотности на прямые (без обращения к базовым)
+1. переделать функции обратные температуры, давления, плотности на прямые (без обращения к базовым) (DONE)
 2. взаимосвязь лямбда - мах (+ обратная)
-3. проверить правильность функций на таблицах (к = 1.4)
-4. добавить описание к каждой функции через breakpoint() (или циклами)
-5. начальное приближение для обратной газодинамической функции расхода
+3. проверить правильность функций на таблицах (к = 1.4) (циклом)
+4. добавить описание к каждой функции через 
+5. начальное приближение для обратной газодинамической функции расхода (DONE)
 """
 
 
@@ -30,7 +32,7 @@ def tau(la, k):
 
 def eps(la, k):
     x = 1.0-(k-1.0)/(k+1.0)*la*la
-    x = math.pow(x,1.0/(k-1.0))
+    x = math.pow(x, 1.0/(k-1.0))
     return x
 
 
@@ -50,9 +52,10 @@ def M(la, k):
     return x
 
 
-def qr(qq, k, la):
+def qrsub(qq, k):
+    la = qq
     num = 0.0000001
-    while math.fabs(q(la, k) - qq) > num:
+    while math.fabs((q(la, k) - qq) / qq) > num:
         dydx = (q(la, k) - q(la + num, k)) / num
         la = la + (q(la, k) - qq) / dydx
     if la > 1.0:
@@ -60,37 +63,34 @@ def qr(qq, k, la):
     return la
 
 
-def pir(pipi, k, la):
+def qrsup(qq, k):
+    la = (1 - qq) * (np.power((k+1)/(k-1), 0.5) - 1) + 1
     num = 0.0000001
-    while math.fabs(pi(la, k) - pipi) > num:
-        dydx = (pi(la, k) - pi(la + num, k)) / num
-        la = la + (pi(la, k) - pipi) / dydx
-    if la > 1.0:
-        print('la(pi) > 1')
+    while math.fabs((q(la, k) - qq) / qq) > num:
+        dydx = (q(la, k) - q(la + num, k)) / num
+        la = la + (q(la, k) - qq) / dydx
+    if la < 1.0:
+        print('la(q) < 1')
     return la
 
 
-def taur(tautau, k, la):
-    num = 0.0000001
-    while math.fabs(tau(la, k) - tautau) > num:
-        dydx = (tau(la, k) - tau(la + num, k)) / num
-        la = la + (tau(la, k) - tautau) / dydx
-    if la > 1.0:
-        print('la(tau) > 1')
-    return la
+def pir(pipi, k):
+    la = (k + 1) / (k - 1) * (1 - np.power(pipi, (k-1)/k))
+    return np.sqrt(la)
 
 
-def epsr(epseps, k, la):
-    num = 0.0000001
-    while math.fabs(eps(la, k) - epseps) > num:
-        dydx = (eps(la, k) - eps(la + num, k)) / num
-        la = la + (eps(la, k) - epseps) / dydx
-    if la > 1.0:
-        print('la(eps) > 1')
-    return la
+def taur(tautau, k):
+    la = (k+1) / (k-1) * (1 - tautau)
+    return np.sqrt(la)
+
+
+def epsr(epseps, k):
+    la = (k + 1) / (k - 1) * (1 - np.power(epseps, (k-1)))
+    return np.sqrt(la)
 
 
 def mk(k, R):
     x = math.sqrt(k/R*math.pow(2.0/(k+1.0),(k+1.0)/(k-1.0)))
     return x
 
+breakpoint()
